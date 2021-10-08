71b8b64f9a3e34f3b401c6f78284d296496da644
ebereoliver <91791105+ebereoliver@users.noreply.github.com>
2021-10-08 16:14:08 +0300

    Add files via upload

---
+++ PROJECT.C
@@ -0,0 +1,15 @@
+/*Arithmetic Operatorsdivision-User friendly Program*/
+#include,<stdio.h>
+int main()
+{
+    int x,y,division;
+    printf("\n Enter value to x:");
+    scanf("%d",&X);
+    printf("/n Enter value to y:");
+    scanf("%d",&y);
+    division=x/y;
+    printf("\n DIVISION=%d",division);
+    return 0;
+}
+
+
