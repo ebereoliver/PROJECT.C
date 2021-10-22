7c5036e04989d854a5f8e2321ee5bce93adf48ae
ebereoliver <91791105+ebereoliver@users.noreply.github.com>
2021-10-22 16:49:17 +0300

    Add files via upload

---
+++ LOGICAL OPERATORS.C.C
@@ -0,0 +1,25 @@
+/*LOGICAL OPERATORS*/
+#include <stdio.h>
+int read()
+{
+    int a, b, c, d;
+    printf("enter value a, b, c, d");
+    scanf("%d, %d, %d, %d", &a, &b, &c, &d);
+
+    if(a>b&&a!=0)
+    {
+     printf("&& operator:both conditions are true\n");
+    }
+
+
+    if(c>d||d!=c)
+    {
+     printf("|| operator:only one condition is true\n");
+    }
+
+    if(!(a>b&&a!=0)){
+     printf("! operator:both conditions are true"
+            "but status is inverted to false");
+    }
+
+    }
